95921d4400d4c6f51461a4e466ce18ebd24b638c
Coderinsightmaven <lmarincik@proton.me>
2024-03-27 02:23:37 -0600

    added a header for the main page with my posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -227,8 +227,9 @@ export default async function Home() {
       </Container>
       <Photos />
       <Container className="mt-24 md:mt-28">
+      <h1 className='text-lg font-bold tracking-tight text-zinc-800 dark:text-zinc-100'>My Recent Posts</h1>
         <div className="mx-auto grid max-w-xl grid-cols-1 items-center justify-center gap-y-20 lg:max-w-none  lg:grid-cols-2">
-          <div className="flex flex-col items-center">
+           <div className="flex flex-col items-center">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
